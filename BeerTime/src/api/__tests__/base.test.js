b6aa727a2dcd04a7f5ee48d83763444ea3388384
Fernando Basello <>
2019-07-18 08:11:28 -0300

    Test BaseApi error cases

--- BeerTime/src/api/__tests__/base.test.js
+++ BeerTime/src/api/__tests__/base.test.js
@@ -2,9 +2,15 @@
 import apiAction from '../base';
 
 describe('base api test suite', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
   it('should dispatch correct actions if response is correct', () => {
     const next = jest.fn();
     const dispatch = jest.fn();
+    const body = JSON.stringify({ data: 'data' });
+    fetch.once(body, { status: 200 });
     return apiAction({ type: 'action', request: { url: 'url' } }, next)(dispatch).then(() => {
       expect(dispatch).toHaveBeenNthCalledWith(1,
         {
@@ -13,13 +19,29 @@ describe('base api test suite', () => {
         });
       expect(dispatch).toHaveBeenNthCalledWith(2,
         {
-          error: undefined, request: { url: 'url' }, type: 'action', value: '',
+          error: undefined, request: { url: 'url' }, type: 'action', value: { data: 'data' },
         });
       expect(dispatch).toHaveBeenNthCalledWith(3,
         {
           key: 'action',
           type: 'API_TRANSACTION_END',
         });
+    });
+  });
+
+  it('should dispatch correct actions if response is error', () => {
+    const next = jest.fn();
+    const dispatch = jest.fn();
+    fetch.once('', { status: 400 });
+
+    return apiAction({ type: 'action', request: { url: 'url' } }, next)(dispatch).then(() => {
+      expect(dispatch).toHaveBeenNthCalledWith(1,
+        {
+          key: 'action',
+          type: 'API_TRANSACTION_BEGIN',
+        });
+      expect(dispatch).toHaveBeenNthCalledWith(2,
+        expect.objectContaining({ key: 'action', type: 'API_TRANSACTION_ERROR' }));
     });
   });
 });
