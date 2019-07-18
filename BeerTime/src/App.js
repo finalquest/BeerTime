e3a1a21e700094978e5df476b7ceef2dc8c3b66a
Fernando Basello <>
2019-07-17 23:58:32 -0300

    SafeAreaView for Iphone X

--- BeerTime/src/App.js
+++ BeerTime/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
-
 import store from './store';
 import AppNavigator from './containers/AppNavigator';
 
 const App = () => (
   <Provider store={store}>
-    <AppNavigator />
+    <SafeAreaView style={{ flex: 1, alignSelf: 'stretch' }}>
+      <AppNavigator />
+    </SafeAreaView>
   </Provider>
 );
 
