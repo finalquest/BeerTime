0ef794debccc3e72fbb49210d9a6cd93309e7a9e
Fernando Basello <>
2019-07-18 22:05:11 -0300

    FilterInput styles and select button

--- BeerTime/src/components/FilterInput.js
+++ BeerTime/src/components/FilterInput.js
@@ -1,31 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 import {
   View,
   StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 
+import PropTypes from 'prop-types';
+
+import TextInput from './TextInput';
 import Label from './Label';
 
 const styles = StyleSheet.create({
-  container: { backgroundColor: 'white' },
+  container: {
+    backgroundColor: 'white',
+    alignSelf: 'stretch',
+    marginHorizontal: 20,
+    padding: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: 'lightgray',
+    marginBottom: 5,
+  },
+  inputLabel: {
+    fontSize: 12,
+    marginTop: 5,
+    marginBottom: 5,
+    color: 'dimgrey',
+  },
+  title: {
+    fontSize: 18,
+    textAlign: 'left',
+    fontWeight: '400',
+    marginBottom: 20,
+  },
+  primary: {
+    backgroundColor: '#4169ff',
+    height: 45,
+    alignSelf: 'stretch',
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  text: {
+    color: 'white',
+    fontSize: 18,
+  },
 });
 
-const FilterInput = () => (
-  <View style={styles.container}>
-    <Label>
-    Testing
-    </Label>
-  </View>
-);
+const FilterInput = ({ onFilterSelected }) => {
+  const [inputValue, setInputValue] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Label style={styles.title}>Search</Label>
+      <TextInput
+        label="Beer Name"
+        style={styles.input}
+        labelStyle={styles.inputLabel}
+        value={inputValue}
+        onChange={(text) => {
+          setInputValue(text);
+        }}
+      />
+      <View>
+        <TouchableOpacity
+          style={styles.primary}
+          onPress={() => {
+            onFilterSelected();
+          }}
+        >
+          <Label style={styles.text}>OK</Label>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 FilterInput.propTypes = {
-
+  onFilterSelected: PropTypes.func.isRequired,
 };
 
 FilterInput.defaultProps = {
-
 };
 
 export default FilterInput;
--- BeerTime/src/components/__tests__/FilterInput.test.js
+++ BeerTime/src/components/__tests__/FilterInput.test.js
@@ -4,8 +4,25 @@ import { shallow } from 'enzyme';
 import FilterInput from '../FilterInput';
 
 describe('FilterInput test suite', () => {
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, 'useState');
+  useStateSpy.mockImplementation(init => [init, setState]);
+
   it('renders as expected', () => {
     const wrapper = shallow(<FilterInput />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should set inputvalue state on textInput onChange', () => {
+    const wrapper = shallow(<FilterInput />);
+    wrapper.find('TextInput').prop('onChange')('test');
+    expect(setState).toBeCalledWith('test');
+  });
+
+  it('should call callback if ok button pressed', () => {
+    const selected = jest.fn();
+    const wrapper = shallow(<FilterInput onFilterSelected={selected} />);
+    wrapper.find('TouchableOpacity').prop('onPress')();
+    expect(selected).toBeCalled();
   });
 });
