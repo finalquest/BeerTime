3b302ae951f815c3036aa7d842ba285c11a8c399
Fernando Basello <>
2019-07-20 11:45:36 -0300

    Convert detail screent to scrollview. There are some descriptions that are too long

--- BeerTime/src/screens/BeerDetailScreen.js
+++ BeerTime/src/screens/BeerDetailScreen.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   View,
   StyleSheet,
+  ScrollView,
 } from 'react-native';
 
 import PropTypes from 'prop-types';
@@ -55,6 +56,11 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     overflow: 'hidden',
   },
+  scrollView: {
+    marginHorizontal: 10,
+    flex: 1,
+    alignSelf: 'stretch',
+  },
 });
 
 const BeerDetailScreen = ({
@@ -63,49 +69,50 @@ const BeerDetailScreen = ({
   },
 }) => (
   <View style={styles.container}>
-    <View style={{ alignSelf: 'stretch' }}>
-      <BackArrow onPress={onBack} />
-      <View style={{ marginHorizontal: 10 }}>
-        <Label style={styles.title}>{name}</Label>
-        <Label style={styles.subTitle}>{tagline}</Label>
-        <View style={styles.descriptionContainer}>
-          <Label style={[styles.description, { marginVertical: 5 }]}>{description}</Label>
-        </View>
-        <View style={styles.volumeContainer}>
-          <PropertyLabel
-            name="ABV"
-            style={{ marginVertical: 5 }}
-            numberOfLines={0}
-            nameStyles={[styles.description, { fontWeight: '600' }]}
-            valueStyles={styles.description}
-            value={abv}
-            separator=":"
-          />
-          <PropertyLabel
-            name="Volume"
-            style={{ marginVertical: 5 }}
-            numberOfLines={0}
-            nameStyles={[styles.description, { fontWeight: '600' }]}
-            valueStyles={styles.description}
-            value={`${value} ${unit}`}
-            separator=":"
-          />
-        </View>
-        {foodPairing.length > 0 ? (
-          <View style={styles.descriptionContainer}>
-            <PropertyLabel
-              name="Food Pairing"
-              style={{ marginVertical: 5 }}
-              numberOfLines={0}
-              nameStyles={[styles.description, { fontWeight: '600' }]}
-              valueStyles={styles.description}
-              value={foodPairing.join('\n')}
-              separator=":"
-            />
-          </View>
-        ) : undefined}
+    <BackArrow onPress={onBack} />
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={styles.scrollView}
+    >
+      <Label style={styles.title}>{name}</Label>
+      <Label style={styles.subTitle}>{tagline}</Label>
+      <View style={styles.descriptionContainer}>
+        <Label style={[styles.description, { marginVertical: 5 }]}>{description}</Label>
       </View>
-    </View>
+      <View style={styles.volumeContainer}>
+        <PropertyLabel
+          name="ABV"
+          style={{ marginVertical: 5 }}
+          numberOfLines={0}
+          nameStyles={[styles.description, { fontWeight: '600' }]}
+          valueStyles={styles.description}
+          value={abv}
+          separator=":"
+        />
+        <PropertyLabel
+          name="Volume"
+          style={{ marginVertical: 5 }}
+          numberOfLines={0}
+          nameStyles={[styles.description, { fontWeight: '600' }]}
+          valueStyles={styles.description}
+          value={`${value} ${unit}`}
+          separator=":"
+        />
+      </View>
+      {foodPairing.length > 0 ? (
+        <View style={styles.descriptionContainer}>
+          <PropertyLabel
+            name="Food Pairing"
+            style={{ marginVertical: 5 }}
+            numberOfLines={0}
+            nameStyles={[styles.description, { fontWeight: '600' }]}
+            valueStyles={styles.description}
+            value={foodPairing.join('\n')}
+            separator=":"
+          />
+        </View>
+      ) : undefined}
+    </ScrollView>
   </View>
 );
 
